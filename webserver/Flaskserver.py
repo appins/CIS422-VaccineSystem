11fec1218efa60fc874b4f5498e9dc1245c7d79f
CSTrebbien <trebbiencolton@gmail.com>
2021-04-16 16:41:39 -0700

    updated flask test run

--- webserver/Flaskserver.py
+++ webserver/Flaskserver.py
@@ -1,1 +1,13 @@
-#Git Test
+"""
+PUT THE DOC STUFF HERE
+"""
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def Home():
+	return "Hello This is a test page <h1>HELLO<h1>"
+
+if __name__ == "__main__":
+	app.run()
