fc7ac7e728d26b4086d8231ce9f6dd37e1aef52f
Alex Anderson <alexandersonone@gmail.com>
2021-04-16 15:37:25 -0700

    Create migration file and migrate

--- database/db.py
+++ database/db.py
@@ -9,7 +9,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class Vaccinee(db.Model):
---
+++ database/migrate.py
@@ -0,0 +1,12 @@
+'''
+CIS 422 Project 1: Vaccination System
+Group 1: Alex Anderson, Hans Prieto, Vince Qiu, Colton Trebbien, Michael Wiltshire
+
+This file does all migrations needed to create the datbase
+'''
+
+from db import db
+
+db.create_all()
+
+
