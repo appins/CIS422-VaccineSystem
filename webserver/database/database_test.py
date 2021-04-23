a13f9e7760a47859d04d028687a26f351223c38c
Michael Wiltshire <mikulwinter@gmail.com>
2021-04-23 15:15:28 -0700

    updated db creation and deletion testing

--- webserver/database/database_test.py
+++ webserver/database/database_test.py
@@ -12,19 +12,38 @@ migrate.hard_reset()
 
 class T1_users(unittest.TestCase):
 
-	def test_create__get_delete_user(self):
+	def test_create_delete_user(self):
 		print("check user")
 		#checks create user
 		self.assertEqual(db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
 												1234567890, 7777, "password123"),True)
-		#checks get user
-		self.assertIsNotNone(db.get_user_by_username("hi"))
-		self.assertEqual(len(db.debug_get_all_users()), 1)
+		#checks if you can create user with same full name
+		self.assertEqual(db.create_vaccinee("hey", "Hi Hello", "hey@hi.hey",
+						   				1234567891, 1111, "password123"), True)
 
 		#check delete user
+		db.create_vaccinee("hey", "Hey Hello", "hey@hi.hey",
+						   1234567891, 1111, "password123")
 		self.assertTrue(db.delete_user("hi"))
+		self.assertTrue(db.delete_user("hey"))
 		self.assertEqual(len(db.debug_get_all_users()), 0)
 
+	def test_get_user(self):
+		#checks get user
+		db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
+						   1234567890, 7777, "password123")
+		db.create_vaccinee("hey", "Hey Hello", "hey@hi.hey",
+						   1234567891, 1111, "password123")
+
+		self.assertEqual(str(db.get_user_by_username("hi")),"<Vaccinee 1>")
+		self.assertEqual(str(db.get_user_by_username("hey")),"<Vaccinee 2>")
+		self.assertEqual(len(db.debug_get_all_users()), 2)
+		print("here:",db.debug_get_all_users())
+		self.assertTrue(db.delete_user("hi"))
+		self.assertEqual(str(db.get_user_by_username("hi")), "None")
+		self.assertTrue(db.delete_user("hey"))
+		self.assertEqual(str(db.get_user_by_username("hey")), "None")
+		self.assertEqual(len(db.debug_get_all_users()), 0)
 	def test_duplicates(self):
 		db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
 							1234567890, 7777, "password123")
@@ -57,6 +76,8 @@ class T1_users(unittest.TestCase):
 		self.assertEqual(str(db.generate_call_list(1)),"[<Vaccinee 3>]")
 		self.assertTrue(db.delete_user("hola"))
 		self.assertEqual(str(db.generate_call_list(1)),"[<Vaccinee 1>]")
-
+		self.assertTrue(db.delete_user("hi"))
+		self.assertTrue(db.delete_user("hey"))
+		self.assertEqual(len(db.debug_get_all_users()), 0)
 if __name__ == '__main__':
     unittest.main()
