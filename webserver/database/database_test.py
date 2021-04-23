20b960230505ac011fd491be71999f1835c725ff
Michael Wiltshire <mikulwinter@gmail.com>
2021-04-23 14:57:04 -0700

    db testing return highest score

--- webserver/database/database_test.py
+++ webserver/database/database_test.py
@@ -12,38 +12,51 @@ migrate.hard_reset()
 
 class T1_users(unittest.TestCase):
 
-		def test_create__get_delete_user(self):
-				print("check user")
-				#checks create user
-				self.assertEqual(db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
-														1234567890, 7777, "password123"),True)
-				#checks get user
-				self.assertIsNotNone(db.get_user_by_username("hi"))
-				self.assertEqual(len(db.debug_get_all_users()), 1)
+	def test_create__get_delete_user(self):
+		print("check user")
+		#checks create user
+		self.assertEqual(db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
+												1234567890, 7777, "password123"),True)
+		#checks get user
+		self.assertIsNotNone(db.get_user_by_username("hi"))
+		self.assertEqual(len(db.debug_get_all_users()), 1)
 
-				#check delete user
-				self.assertTrue(db.delete_user("hi"))
-				self.assertEqual(len(db.debug_get_all_users()), 0)
+		#check delete user
+		self.assertTrue(db.delete_user("hi"))
+		self.assertEqual(len(db.debug_get_all_users()), 0)
 
-		def test_duplicates(self):
-			db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
-									1234567890, 7777, "password123")
+	def test_duplicates(self):
+		db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
+							1234567890, 7777, "password123")
 
-			#check duplicate username
-			self.assertFalse(db.create_vaccinee("hi", "Hello", "Hola@hi.hey",
-									1234567891, 7777, "password123"))
+		#check duplicate username
+		self.assertFalse(db.create_vaccinee("hi", "Hello", "Hola@hi.hey",
+								1234567891, 7777, "password123"))
 
-			#check duplicate email
-			self.assertFalse(db.create_vaccinee("Hoi", "Henlo Hoi", "hello@hi.hey",
-												1234567892, 7777, "password123"))
+		#check duplicate email
+		self.assertFalse(db.create_vaccinee("Hoi", "Henlo Hoi", "hello@hi.hey",
+											1234567892, 7777, "password123"))
 
-			self.assertFalse(db.create_vaccinee("eyyy", "Henlo Hoi", "heyo@hi.hey",
-												1234567890, 7777, "password123"))
+		self.assertFalse(db.create_vaccinee("eyyy", "Henlo Hoi", "heyo@hi.hey",
+											1234567890, 7777, "password123"))
 
-			self.assertEqual(len(db.debug_get_all_users()), 1)
+		self.assertEqual(len(db.debug_get_all_users()), 1)
+		self.assertTrue(db.delete_user("hi"))
+		self.assertEqual(len(db.debug_get_all_users()), 0)
 
+	def test_scores(self):
+		#checks to see if you can create users with different scores,
+		#and checks highest score is returned first
+		db.create_vaccinee("hi", "Hi Hello", "hello@hi.hey",
+						   1234567890, 2222, "password123")
+		db.create_vaccinee("hey", "Hey Hello", "hey@hi.hey",
+						   1234567891, 1111, "password123")
+		db.create_vaccinee("hola", "Hola Hello", "hola@hi.hey",
+						   1234567892, 9999, "password123")
 
-
+		self.assertEqual(str(db.generate_call_list(1)),"[<Vaccinee 3>]")
+		self.assertTrue(db.delete_user("hola"))
+		self.assertEqual(str(db.generate_call_list(1)),"[<Vaccinee 1>]")
 
 if __name__ == '__main__':
     unittest.main()
