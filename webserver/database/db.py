1439c8a2ff35bad700bc1a602708ed4e258e5eb9
Alex Anderson <alexandersonone@gmail.com>
2021-04-23 16:52:00 -0700

    Add comments to db.py

--- webserver/database/db.py
+++ webserver/database/db.py
@@ -38,6 +38,7 @@ class Vaccinee(db.Model):
         - A score based on health factors
         - A password
         - If they've been vaccinated
+        - And a date on which the user registered (auto-generated)
     All of this information is required
     '''
     id = db.Column(db.Integer, primary_key=True)
@@ -73,7 +74,11 @@ def create_vaccinee(fullname: str, email: str, phone: str, score: float, passwor
 
 def get_user_by_email(email: str) -> Vaccinee:
     '''Select a user based on their email'''
+    
+    # Search for a user by their email, return only the first result
+    # (should be the only result due to unique keys)
     user = Vaccinee.query.filter_by(email=email).first()
+
     return user
 
 def delete_user(email: str) -> bool:
@@ -82,15 +87,25 @@ def delete_user(email: str) -> bool:
 
     Returns true on success and false on failure.
     '''
+
+    # We start by finding the user
     user = Vaccinee.query.filter_by(email=email).first()
+
     if user is None:
+        # In the case that there is no user by that email, we return false
+        # as it was a failure
         return False
     else:
+        # If we could find a user, we attempt to delete them
         try:
+            # if it works we return true
             db.session.delete(user)
             db.session.commit()
             return True
         except:
+            # If there was some sort of error we return false. No
+            # need to completely halt the server, however, because
+            # there could be a good reason (such as write-protection)
             db.session.rollback()
             return False
 
@@ -99,26 +114,36 @@ def generate_call_list(n: int) -> Optional[List[Vaccinee]]:
 
     Returns None if there are any errors.
     '''
-    users = Vaccinee.query.filter_by(has_been_vaccinated=False
-            ).order_by(Vaccinee.score.desc(), Vaccinee.date_created
-            ).limit(n).all()
 
+    # We select the first n users, sorted by health risk score (highest first)
+    # and then by date. 
+    users = Vaccinee.query.filter_by(has_been_vaccinated=False).
+            order_by(Vaccinee.score.desc(), Vaccinee.date_created).
+            limit(n).all()
+
+    # Set all of the users as being vaccinated
     for user in users:
         user.has_been_vaccinated=True
-
     try:
+        # Commit the changes (that is, store that these users have been vaccinated)
         db.session.commit()
     except:
+        # If we fail to commit these changes, rollback (in order to not halt webserver
+        # operation.
+        db.session.rollback()
         return None
 
     return users
-
 
 def debug_get_all_users() -> list:
     '''Get all users returns a list of all of the registered users. Useful for
     debugging.
 
-    Don't use this in production.
+    Don't use this in production. It queries the whole database and therefore
+    could crash the server if it gets called on a huge database.
     '''
+
+    # Search for all vaccinees, vaccinated or unvaccinated, and return them.
+    # particularly useful in testing.
     return Vaccinee.query.all()
 
